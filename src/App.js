00750e5e5b5ea047581df79377c851b312548250
s-udaka <1s1h1o9@gmail.com>
2020-09-26 13:12:30 +0000

    control style

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import TodoList from './TodoListComponent';
 import Form from './FormComponent';
+import './App.css';
 
 class App extends React.Component {
     constructor(props) {
@@ -29,7 +30,7 @@ class App extends React.Component {
 
     render() {
         return (
-            <div>
+            <div className="app">
                 <h1>TODOアプリ</h1>
                 <Form value={this.state.value} handleChange={this.handleChange} handleSubmit={this.handleSubmit} />
                 <TodoList list={this.state.list} />
